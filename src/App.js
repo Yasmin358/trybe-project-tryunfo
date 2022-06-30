d0908800fa29d49fbe8b2d7098311098624202a8
Yasmin358 <amorin.yasmin378@gmail.com>
2022-06-30 16:08:12 -0300

    Resolução requisito 3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import Form from './components/Form';
+import Card from './components/Card';
 
 class App extends React.Component {
   render() {
     return (
-      <div id="left">
-        <h1>Tryunfo!</h1>
-        <Form />
+      <div>
+        <div id="left">
+          <h1>Tryunfo!</h1>
+          <Form />
+        </div>
+        <div id="right">
+          <Card />
+        </div>
       </div>
     );
   }
